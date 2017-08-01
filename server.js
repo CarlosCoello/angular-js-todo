5cf41886a8a9c2923eb8b9e12f5560771f2af8c1
Carlos Coello <carlos.coello05@outlook.com>
2017-08-01 00:37:53 -0500

    changes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 1212);
 
-const forceSSL = () => {
-  return (req, res, next){
+const forceSSL = function(){
+  return function(req, res, next){
     if(req.headers['x-forwarded-proto'] !== 'https'){
       return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
